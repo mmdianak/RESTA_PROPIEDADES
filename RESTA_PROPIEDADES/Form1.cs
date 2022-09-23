cb6b0e997c9ae0db8d3e7fb5bbbe931f8f3a315a
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-23 02:21:55 -0600

    Agregar archivos de proyecto.

---
+++ RESTA_PROPIEDADES/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RESTA_PROPIEDADES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Instancia de la clase cloperaciones.
+            RESTA oresta = new RESTA();
+            // Se le asigna a la propiedad N1 el valor dado en la textBox1.
+            oresta.N1 = int.Parse(textBox1.Text);
+            // Se le asigna a la propiedad N2 el valor dado en la textBox2.
+            oresta.N2 = int.Parse(textBox2.Text);
+            // Se le asigna a la textBox3 el resultado que se le aplica al objeto oresta con el metodo Restar y se pasa a tipo string.
+            textBox3.Text = oresta.Restar().ToString();
+        }
+    }
+}
---
+++ RESTA_PROPIEDADES/RESTA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTA_PROPIEDADES
+{
+    internal class RESTA
+    {
+        // Propiedades, campos, etc.
+        private int n1;
+        private int n2;
+
+        // Encapsulamiento de las propiedades.
+        public int N1 { get => n1; set => n1 = value; }
+        public int N2 { get => n2; set => n2 = value; }
+
+        // Metodo
+
+        public int Restar()
+        {
+            return n1 - n2;
+        }
+    }
+}
